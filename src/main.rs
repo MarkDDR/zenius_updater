a45eed987efdbed6dcb3beda6061cf5d7f4a6b4e
Mark Blum <mark.n.blum@gmail.com>
2021-02-23 03:30:04 -0500

    This exhibits rustc eating my memory until OoM when I try to compile

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
-use std::error::Error;
+use std::{collections::HashMap, path, time::Duration};
 
+use path::PathBuf;
 use select::{
     document::Document,
     predicate::{Attr, Name, Predicate},
 };
 
+use anyhow::{Context, Result};
+use tokio::fs::read_dir;
+
 // TODO custom error type with thiserror or snafu or something
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let client = reqwest::Client::new();
+async fn main() -> Result<()> {
+    let a20_path = "/home/mark/.stepmania-5.1/Songs/DDR A20\ PLUS";
+    let blah = walk_song_folder(a20_path).await?;
+
+    // let client = reqwest::Client::new();
+    let client = reqwest::Client::builder()
+        .timeout(Duration::from_secs(5))
+        .build()
+        .unwrap();
     let a20_plus_url = "https://zenius-i-vanisher.com/v5.2/viewsimfilecategory.php?categoryid=1293";
+    println!("Sending request to zenius...");
     let res_body = client.get(a20_plus_url).send().await?.text().await?;
+    println!("Got Response back from zenius!");
     let document = Document::from(res_body.as_str());
 
     for potential_song_node in document.find(Name("tr")) {
-        let (song_name, song_url, last_updated_txt) = match extract_song_info(potential_song_node) {
+        let songinfo = match extract_song_info(potential_song_node) {
             Some(x) => x,
             None => continue,
         };
 
-        println!(
-            "song_name: {}, {} -> {}",
-            song_name, last_updated_txt, song_url
-        );
+        println!("{:?}", songinfo);
     }
     Ok(())
 }
 
-fn extract_song_info(potential_song_node: select::node::Node) -> Option<(String, &str, String)> {
+#[derive(Clone, Debug)]
+struct SongInfoZenius {
+    song_name: String,
+    url: String,
+    last_updated: String,
+}
+
+fn extract_song_info(potential_song_node: select::node::Node) -> Option<SongInfoZenius> {
     // At time of writing (2020-02-19), the parts of the song html we care about looked like so
     // <tr>
     //   <td>
@@ -59,7 +76,8 @@ fn extract_song_info(potential_song_node: select::node::Node) -> Option<(String,
 
     let song_name = song_node.text();
     // TODO check it is in the form of viewsimfile.php?simfileid=12345
-    let song_url = song_node.attr("href").unwrap();
+    let url_ending = song_node.attr("href").unwrap();
+    let url = format!("https://zenius-i-vanisher.com/v5.2/{}", url_ending);
 
     let last_updated = td_last_updated.find(Name("span")).next()?;
     let last_updated_txt = last_updated.text();
@@ -68,6 +86,34 @@ fn extract_song_info(potential_song_node: select::node::Node) -> Option<(String,
     // the actual song page has a better datetime we could use there, but web calls
     // to zenius are really slow so we want to avoid it if we can
 
-    // TODO make some kind of SongInfo struct to store these in
-    Some((song_name, song_url, last_updated_txt))
+    Some(SongInfoZenius {
+        song_name,
+        url,
+        last_updated: last_updated_txt,
+    })
+}
+
+struct SongInfoFS {
+    song_name: String,
+    last_updated: Duration,
+    path: PathBuf,
+    components: HashMap<String, Duration>,
+}
+
+async fn walk_song_folder(version_folder: &str) -> Result<HashMap<String, SongInfoFS>> {
+    // let mut songs = HashMap::new();
+    let mut walker = read_dir(version_folder)
+        .await
+        .context("invalid song folder path")?;
+    while let Some(entry) = walker.next_entry().await? {
+        let file_type = entry.file_type().await?;
+        if !file_type.is_dir() {
+            continue;
+        }
+        println!("{:?}", entry.path());
+        continue;
+        let mut song_walker = read_dir(entry.path()).await?;
+        while let Some(component) = song_walker.next_entry().await? {}
+    }
+    todo!()
 }
